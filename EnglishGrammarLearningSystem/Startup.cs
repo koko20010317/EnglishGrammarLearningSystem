eda802e0025121e0a0192bb2f7e457776759b4b6
Cathy <Cathy@CATHYCHUNG>
2021-11-07 22:17:02 +0800

    加入專案檔案。

---
+++ EnglishGrammarLearningSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EnglishGrammarLearningSystem.Startup))]
+namespace EnglishGrammarLearningSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
